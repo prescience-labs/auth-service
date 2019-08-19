722ca0765b7638b418435094a5e8d5fbd6ba8ed1
Jacob Dick <jdick@solutionreach.com>
2019-08-19 07:00:19 -0600

    remove token serializer

--- app/serializers/__init__.py
+++ app/serializers/__init__.py
@@ -3,5 +3,4 @@
 Serializers for the Django Rest Framework
 https://www.django-rest-framework.org/api-guide/serializers/
 """
-from .token import TokenSerializer
 from .user import UserSerializer
--- app/serializers/token.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from app.services.jwt import JWT
-
-class TokenSerializer(serializers.Serializer):
-    token = serializers.CharField(read_only=True)
-
-    def __init__(self, user):
-        self.token = JWT.get_user_token(user)

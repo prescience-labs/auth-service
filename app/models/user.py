6d47e4d2b4c51efa988a625696bb2bab13eca20c
Jacob Dick <jdick@solutionreach.com>
2019-08-19 06:41:59 -0600

    add username field

--- app/models/user.py
+++ app/models/user.py
@@ -7,10 +7,15 @@ from django.contrib.auth.models import AbstractUser
 from django.db.models.signals import pre_save
 from django.db import models
 from django.dispatch import receiver
+from django.utils.translation import gettext_lazy as _
 
 class User(AbstractUser):
     """Custom User Model"""
     uid = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
+    email = models.EmailField(_('email address'), unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
 
 #pylint: disable=unused-argument
 @receiver(pre_save, sender=User)

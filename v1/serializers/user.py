d8c319127f23074238adc1017b5a92d865fbe652
Jacob Dick <jdick@solutionreach.com>
2019-10-15 07:01:05 -0600

    add user to a team on user creation

--- v1/serializers/user.py
+++ v1/serializers/user.py
@@ -1,13 +1,23 @@
+import logging
+
+from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 from common.models import Team
 from .team import TeamSerializer
 
-User = get_user_model()
+logger  = logging.getLogger(__name__)
+User    = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
-    teams = serializers.HiddenField(default=None)
+    team    = serializers.UUIDField(required=False, default=None)
+    teams   = serializers.SerializerMethodField('get_teams')
+
+    def get_teams(self, obj):
+        """Forces the `teams` field to return a list of IDs"""
+        teams_queryset = Team.objects.filter(users__id=obj.id)
+        return [t.id for t in teams_queryset]
 
     class Meta:
         model   = User
@@ -16,6 +26,7 @@ class UserSerializer(serializers.ModelSerializer):
             'email',
             'password',
             'is_active',
+            'team',
             'teams',
             'created_at',
             'updated_at',
@@ -23,6 +34,7 @@ class UserSerializer(serializers.ModelSerializer):
         read_only_fields = [
             'id',
             'is_active',
+            'teams',
             'created_at',
             'updated_at',
         ]
@@ -31,11 +43,22 @@ class UserSerializer(serializers.ModelSerializer):
         }
 
     def create(self, validated_data):
-        return User.objects.create_user(
+        user = User.objects.create_user(
             email=validated_data['email'],
             username=validated_data['email'],
             password=validated_data['password'],
         )
+
+        # Attempt to add the user to the team.
+        # If no team was listed, or if the user couldn't be added to the team,
+        # create a new team for the user.
+        try:
+            team = Team.objects.get(pk=validated_data['team'])
+        except:
+            team = Team.objects.create(name=f"{user.email}'s Team")
+
+        team.users.add(user)
+        return user
 
 class UserDetailSerializer(UserSerializer):
     teams = serializers.SerializerMethodField('get_teams')

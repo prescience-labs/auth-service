8ba8071fb35fab3990c12168d11f22f4aadfe8a8
Jacob Dick <jdick@solutionreach.com>
2019-10-15 08:03:42 -0600

    fix team add

--- v1/serializers/team.py
+++ v1/serializers/team.py
@@ -6,11 +6,10 @@ from common.models import Team
 User = get_user_model()
 
 class TeamSerializer(serializers.ModelSerializer):
-    users = serializers.HiddenField(default=None)
-
     class Meta:
-        model   = Team
-        fields  = '__all__'
+        model               = Team
+        fields              = '__all__'
+        read_only_fields    = ('users',)
 
 class TeamDetailSerializer(TeamSerializer):
     users = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), many=True)
